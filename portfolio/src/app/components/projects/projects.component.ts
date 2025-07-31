efacd9a26d69efb64712bf8b508b4e4d4139ff19
francisvimal2002 <francisvimal2002@gmail.com>
2025-07-31 12:37:27 +0000

    008 - Projects card design alignment and bugfixes

--- portfolio/src/app/components/projects/projects.component.ts
+++ portfolio/src/app/components/projects/projects.component.ts
@@ -6,9 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
-  projects = [
-    "oands_page",
-    "oands_page",
-    "oands_page",
+  projects: { img: string, title: string }[] = [
+    { img: "'../../../assets/oands_page.jpg'", title: "O and S animation" },
+    { img: "'../../../assets/simon_game.jpg'", title: "Simon Game" },
+    { img: "'../../../assets/dice_game.jpg'", title: "Dice Game" },
+    { img: "'../../../assets/simon_game.jpg'", title: "Drum Music Kit" },
   ]
 }
