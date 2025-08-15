07cfdd9c89b271ab77546e8b58c2e3b5a6fc2bd4
francisvimal2002 <francisvimal2002@gmail.com>
2025-08-15 13:16:09 +0000

    009 - slider adjustments

--- portfolio/src/app/components/projects/projects.component.ts
+++ portfolio/src/app/components/projects/projects.component.ts
@@ -12,4 +12,32 @@ export class ProjectsComponent {
     { img: "'../../../assets/dice_game.jpg'", title: "Dice Game" },
     { img: "'../../../assets/simon_game.jpg'", title: "Drum Music Kit" },
   ]
+
+  dots: number = 0;
+
+  dotsArray: number[] = Array(this.dots);
+
+  nCards: number = 3;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.dots = Math.round(this.projects.length / this.nCards) + 1;
+    console.log(this.dots);
+    this.updateDotsArray();
+  }
+
+  updateDotsArray() {
+    this.dotsArray = Array(this.dots);
+    console.log(this.dotsArray);
+  }
+
+  onDotClick(index: number) {
+    console.log(index);
+    const card: HTMLElement | null = document.querySelector('.projectContentContainer');
+    if(card) {
+      console.log("inside: ", index);
+      card.style.left = `${index * -443}px`;
+    }
+  }
 }
