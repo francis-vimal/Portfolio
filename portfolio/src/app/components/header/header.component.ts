29f21bc697b39432dba8926e78037c98e24c5d74
francisvimal2002 <francisvimal2002@gmail.com>
2025-11-06 14:20:04 +0000

    014 - Responsive changes

--- portfolio/src/app/components/header/header.component.ts
+++ portfolio/src/app/components/header/header.component.ts
@@ -12,9 +12,15 @@ export class HeaderComponent {
     const target = event.currentTarget as HTMLElement;
     const rect = target.getBoundingClientRect();
     const parentRect = target.parentElement!.getBoundingClientRect();
+    const parentStyle = getComputedStyle(target.parentElement!);
+    
+    // Get numeric gap value (e.g. "10px" → 10)
+    const gap = parseFloat(parentStyle.gap) || 0;
     const offset = rect.left - parentRect.left + rect.width / 2;
-  
-    this.animationBlock.nativeElement.style.left = `${offset - 50}px`; // 50 = half of animationBlock width
+    console.log("target:", rect.left, "react", rect.left, "parentRect", parentRect.left, "gap", rect.width / 2);
+    console.log("offset: ", offset);
+    this.animationBlock.nativeElement.style.left = `${offset - gap}px`; // 50 = half of animationBlock width
+    console.log("left: ", offset - gap);
     this.animationBlock.nativeElement.style.transition = `all 0.5s cubic-bezier(0.33, -0.32, 0.5, 1.23)`;
   }
 
